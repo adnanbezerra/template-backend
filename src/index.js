a28098f2122d4ec7deee938085e7bd8a843f8842
Adnan <adnanbezerra@gmail.com>
2022-07-25 19:03:05 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// const PORT = process.env.PORT;
+const PORT = 5000;
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+// Routers session
+server.use()
+
+server.listen(PORT, () => {
+    console.log("It's alive!");
+})
